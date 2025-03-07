0d06f2c10b8afdd12183919aa66e1605bbbea1a7
Yana Ilikbaeva <yanushkay2924@mail.ru>
2025-03-07 11:32:45 +0500

    Обернула в функцию, внесла корректировки

--- data.php
+++ data.php
@@ -1,46 +1,51 @@
 <?php
 
-$year = 2025;
-$month = 3;
 
+function print_month_schedule($year, $month) {
+    // Количество дней в месяце
+    $count_days_in_month = date('t', strtotime("$year-$month-01"));
+    echo "В месяце $count_days_in_month дней\n";
 
-$count_days_in_month = date('t', strtotime("$year-$month-01"));
+    // Определяем первый день недели месяца
+    $day_of_week = date('w', strtotime("$year-$month-01"));
+    $days_of_week = [
+        0 => 'Воскресенье',
+        1 => 'Понедельник',
+        2 => 'Вторник',
+        3 => 'Среда',
+        4 => 'Четверг',
+        5 => 'Пятница',
+        6 => 'Суббота',
+    ];
+    echo "Первый день недели месяца $month - это $days_of_week[$day_of_week]\n";
 
-echo "В месяце $count_days_in_month дней" . PHP_EOL;
+    // Счётчик для работы с цветом
+    $work_counter = 1;
+    echo "Расписание для месяца $month $year:\n";
 
-$day_of_week = date('w', strtotime("$year-$month-01"));
+    // Выводим все дни месяца
+    for ($day = 1; $day <= $count_days_in_month; $day++) {
+        $current_day_of_week = date('w', strtotime("$year-$month-$day"));
 
-$days_of_week = [
-    0 => 'Воскресенье',
-    1 => 'Понедельник',
-    2 => 'Вторник',
-    3 => 'Среда',
-    4 => 'Четверг',
-    5 => 'Пятница',
-    6 => 'Суббота',
-];
-
-echo "Первый день недели месяца $month - это $days_of_week[$day_of_week]" . PHP_EOL;
-
-$work_counter = 1;
-
-echo "Расписание для месяца $month $year:\n";
-for ($day = 1; $day <= $count_days_in_month; $day++) {
-    $current_day_of_week = date('w', strtotime("$year-$month-$day"));
-    if ($work_counter % 3 == 1) {
-        if ($current_day_of_week == 0 || $current_day_of_week == 6) {
-            while ($current_day_of_week == 0 || $current_day_of_week == 6) {
-                $day++;
-                $current_day_of_week = date('w', strtotime("$year-$month-$day"));
+        // Каждый 3 день выводим зеленым
+        if ($work_counter % 3 == 1) {
+            if ($current_day_of_week == 0 || $current_day_of_week == 6) {
+                // Пропускаем выходные
+                while ($current_day_of_week == 0 || $current_day_of_week == 6) {
+                    $day++;
+                    $current_day_of_week = date('w', strtotime("$year-$month-$day"));
+                }
             }
+            echo "\033[32m $day \033[0m" . " "; // Зеленый цвет для рабочих дней
+        } else {
+            echo "\033[31m $day \033[0m" . " "; // Красный цвет для нерабочих дней
         }
-        echo "\033[32m $day \033[0m" . " ";
-    } else {
-        echo "$day" . " ";
+        $work_counter++;
     }
-    $work_counter++;
 }
 
+// Вызываем функцию
+print_month_schedule(2025, 6);
 
 ?>
 
